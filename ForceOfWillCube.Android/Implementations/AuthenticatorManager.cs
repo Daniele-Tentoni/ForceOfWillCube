e85e83ba5f60c61c09af3b5f000115e5e70eb628
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-03-06 23:03:56 +0100

    Xamarin Android project creation.

---
+++ ForceOfWillCube.Android/Implementations/AuthenticatorManager.cs
@@ -0,0 +1,67 @@
+﻿using ForceOfWillCube.Droid.Implementations;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AuthenticatorManager))]
+namespace ForceOfWillCube.Droid.Implementations
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Util;
+    using ForceOfWillCube.Models.Users;
+    using ForceOfWillCube.Utils;
+    using System;
+
+    public class AuthenticatorManager : IAuthenticatorManager
+    {
+        private readonly ISharedPreferences sharedPreferences;
+
+        private UserModel currentUser;
+
+        public AuthenticatorManager()
+        {
+            this.sharedPreferences = Application.Context.GetSharedPreferences("UserInfo", FileCreationMode.Private);
+            this.currentUser = new UserModel
+            {
+                Email = this.sharedPreferences.GetString("user_email", string.Empty),
+                IsLogged = this.sharedPreferences.GetBoolean("is_logged", false),
+                PhotoUrl = this.sharedPreferences.GetString("user_photo_url", string.Empty),
+                Username = this.sharedPreferences.GetString("user_username", "Guest User")
+            };
+        }
+
+        public bool SigninUser(string username)
+        {
+            this.currentUser.Email = username;
+            this.currentUser.IsLogged = true;
+            this.currentUser.PhotoUrl = string.Empty;
+            this.currentUser.Username = username;
+            return this.EditPreferences(this.currentUser);
+        }
+
+        public bool SignoutUser()
+        {
+            this.currentUser = new UserModel { IsLogged = false, Username = "Guest User" };
+            return this.EditPreferences(this.currentUser);
+        }
+
+        private bool EditPreferences(UserModel model)
+        {
+            try
+            {
+                this.sharedPreferences.Edit().PutString("user_email", string.Empty);
+                this.sharedPreferences.Edit().PutBoolean("is_logged", false);
+                this.sharedPreferences.Edit().PutString("user_photo_url", string.Empty);
+                this.sharedPreferences.Edit().PutString("user_username", string.Empty);
+                this.sharedPreferences.Edit().Apply();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Error("AUTHENTICATOR MANAGER", "Catched exception in signin.");
+                throw e;
+            }
+        }
+
+        public UserModel GetSignedUser() => this.currentUser;
+    }
+}
---
+++ ForceOfWillCube.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿namespace ForceOfWillCube.Droid
+{
+    using Android.App;
+    using Android.Content.PM;
+    using Android.Runtime;
+    using Android.OS;
+    using Xamarin.Forms;
+    using ForceOfWillCube.Droid.Implementations;
+    using ForceOfWillCube.Utils;
+
+    [Activity(Label = "ForceOfWillCube", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+
+            DependencyService.Register<IAuthenticatorManager, AuthenticatorManager>();
+            this.LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
