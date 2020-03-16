bd0a6a109512e6105fd1708ee09a71bf37462056
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-03-16 11:22:04 +0100

    Little fix and About page improvements.

--- ForceOfWillCube/Views/AboutViews/AboutPage.cs
+++ ForceOfWillCube/Views/AboutViews/AboutPage.cs
@@ -13,7 +13,7 @@ namespace ForceOfWillCube.Views.AboutViews
                     Padding = new Thickness(10, 5, 10, 5),
                     Children = {
                         new WhiteLabel { Text = "Hi, this is an about page." },
-                        new WhiteLabel { Text = "You can use this app to manage your Force of Will Cube collections." },
+                        new WhiteLabel { Text = "You can use this app to manage your Force of Will Cube collections. To help you build your very first cube, I've prepared a very entry list with every promo card released for a magic light cube experience. But aware of this, you can add every card you want to your cube, but max 1 copy for each card, aware of basic stones. Good game, folks!" },
                         new WhiteLabel { Text = "Don't forget to support the developer with an high rating on the store!" }
                     }
                 }
@@ -25,7 +25,7 @@ namespace ForceOfWillCube.Views.AboutViews
             public WhiteLabel()
             {
                 this.TextColor = Color.White;
-                this.FontSize = 24;
+                this.FontSize = 18;
             }
         }
     }
